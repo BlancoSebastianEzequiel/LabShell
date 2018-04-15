bcc123999d5be70ab75f918bc1a538e69f09d3ca
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-04-15 17:49:33 -0300

    Parte1: Búsqueda en PATH

--- functions.c
+++ functions.c
@@ -3,6 +3,7 @@
 //------------------------------------------------------------------------------
 void execCommand(struct cmd* cmd) {
     struct execcmd* execcmd = (struct execcmd*) cmd;
-    int execvp(execcmd->argv[0], execcmd->argv[0] + 1);
+    char* arguments[] = {NULL};
+    int execvp(execcmd->argv[0], arguments);
 }
 //------------------------------------------------------------------------------
