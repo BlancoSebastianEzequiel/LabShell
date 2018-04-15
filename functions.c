a9d673560756de9d2341329fdeaf4dbc75107398
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-04-15 18:09:01 -0300

    Parte1: Imprimir variables de entorno

--- functions.c
+++ functions.c
@@ -1,8 +1,19 @@
+#include <stdlib.h>
+#include <unistd.h>
 //------------------------------------------------------------------------------
 // EXEC COMMAND
 //------------------------------------------------------------------------------
 void execCommand(struct cmd* cmd) {
     struct execcmd* execcmd = (struct execcmd*) cmd;
     int execvp(execcmd->argv[0], execcmd->argv[0] + 1);
+}
+//------------------------------------------------------------------------------
+// EXPAND ENVIRONMENT VARIABLES
+//------------------------------------------------------------------------------
+static char* expandEnvironmentVariables(char* arg) {
+    if (arg[0] != '$') return arg;
+    char* value = getenv(arg+1);
+    if (value == NULL) return arg;
+    return value;
 }
 //------------------------------------------------------------------------------
